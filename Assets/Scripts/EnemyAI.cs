563c5a63797eb6a213f783b81f5b9a59fdac31bb
Philipp Backes <philippbackes95@gmail.com>
2018-04-12 11:51:06 +0200

    Organized folder structure Added enemy spawn, tower AI and projectile (AI)

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public int hitPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (hitPoints <= 0)
+        {
+            hitPoints = 2;
+        }
+        GameObject targetObject = GameObject.FindGameObjectWithTag("Target");
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = targetObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ReduceHitPoints(int damage)
+    {
+        hitPoints -= damage;
+        if(hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public float spawnTime;
+    public GameObject enemyPrefab;
+
+    private bool spawnEnemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (spawnTime <= 0.0f)
+        {
+            spawnTime = 10.0f;
+        }
+        spawnEnemy = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnEnemy)
+        {
+            Invoke("SpawnEnemy", spawnTime);
+            spawnEnemy = false;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        spawnEnemy = true;
+    }
+}
